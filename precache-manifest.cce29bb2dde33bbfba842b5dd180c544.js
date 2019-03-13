7ac397ca39fd7aac5ee03792c455cbecc6ce1749
heeropunjabi <heeropunjabi@gmail.com>
2019-03-13 21:42:22 +0530

    Updates

---
+++ precache-manifest.cce29bb2dde33bbfba842b5dd180c544.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Happy-Birthday/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d847ec26c9deb0a1f9d0",
+    "url": "/Happy-Birthday/static/js/runtime~main.47d5732e.js"
+  },
+  {
+    "revision": "76a1e898d97cf45dbcc2",
+    "url": "/Happy-Birthday/static/js/main.8033e036.chunk.js"
+  },
+  {
+    "revision": "cfbda0e9061432ca6501",
+    "url": "/Happy-Birthday/static/js/2.f213e47f.chunk.js"
+  },
+  {
+    "revision": "76a1e898d97cf45dbcc2",
+    "url": "/Happy-Birthday/static/css/main.ca70587c.chunk.css"
+  },
+  {
+    "revision": "47a7fa0fb16f55fc8aa76d273f80b900",
+    "url": "/Happy-Birthday/index.html"
+  }
+];
